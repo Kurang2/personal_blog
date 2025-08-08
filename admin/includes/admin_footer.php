388fdb6b9ba9d1d26067016f74449c97c9348988
Kurang2 <kurangkarung02@gmail.com>
2025-08-08 16:22:31 +0700

    Update admin_footer.php

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,1 +1,10 @@
-
+<?php
+// File: /admin/includes/admin_footer.php
+?>
+        </main>
+    </div>
+    <footer class="admin-footer">
+        <p>&copy; <?= date('Y') ?> <?= htmlspecialchars(get_setting('site_title')) ?> - Admin Panel.</p>
+    </footer>
+</body>
+</html>
